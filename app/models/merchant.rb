c115031e552f5b3047589719d8ef9752427fec7d
huyphamily <huyphamily@gmail.com>
2015-11-16 23:05:08 -0800

    validate presence of all field for merchants

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: merchants
+#
+#  id             :integer, not null, primary key
+#  name           :string
+#  description    :text
+#  image          :text
+#  min_amount     :decimal, precision: 8, scale: 2
+#  max_amount     :decimal, precision: 8, scale: 2
+
 class Merchant < ActiveRecord::Base
+  validates :name, :max_amount, :min_amount, :image, :description, presence: true
 end
