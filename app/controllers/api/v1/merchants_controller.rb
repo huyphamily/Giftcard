70d2aed587fd9554e54b5ec1f6a8fe5a7bcc55be
huyphamily <huyphamily@gmail.com>
2015-11-17 01:18:58 -0800

    fix format

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,11 +1,20 @@
 class Api::V1::MerchantsController < ApplicationController
+  before_filter :check_format
 
   def index
     @merchants = Merchant.all
 
     respond_to do |format|
-      format.json { render :json => @merchants }
+      format.json do
+        render_json_response :success, { data: @merchants }
+      end
     end
+  end
+
+  private
+
+  def check_format
+    render nothing: true, status: 406 unless params[:format] == 'json' || request.headers["Accept"] =~ /json/
   end
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def render_json_response(type, hash)
+    unless [ :success, :redirect, :error ].include?(type)
+      raise "Invalid json response type: #{type}"
+    end
+
+    default_json_structure = { 
+      :status => type
+      }.merge(hash)
+
+    render_options = {:json => default_json_structure}  
+    render_options[:status] = 400 if type == :error
+    render_options[:status] = 200 if type == :success
+
+    render(render_options)
+  end
+
 end
